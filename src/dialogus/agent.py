6d22a3787608f83af8c931fb5baf00f42dc8193f
odryfox <odryfox@gmail.com>
2019-11-04 17:37:13 +0300

    Add continuous skills using agent state

--- src/dialogus/agent.py
+++ src/dialogus/agent.py
@@ -1,18 +1,21 @@
-from typing import Callable, List
+from typing import Callable, List, Tuple
 
 
 class Agent:
-    def __init__(self, skill_classifier: Callable[[str], List[Callable[[str], List[str]]]]):
+    def __init__(self, skill_classifier: Callable[[str], List[Callable[[str], Tuple[List[str], int]]]]):
         if not callable(skill_classifier):
             raise TypeError('skill_classifier must be a function')
 
         self.__skill_classifier = skill_classifier
+        self.__state = 0
 
     def answer_me(self, message: str) -> List[str]:
         answers = []
         skills = self.__skill_classifier(message)
 
         for skill in skills:
-            answers += skill(message)
+            skill_result = skill(message, self.__state)
+            self.__state = skill_result[1]
+            answers += skill_result[0]
 
         return answers
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,4 +1,4 @@
-from typing import Callable, List
+from typing import Callable, List, Tuple
 
 import pytest
 
@@ -6,10 +6,10 @@ from src.dialogus.agent import Agent
 
 
 def test_echo_agent():
-    def echo_skill(message: str) -> List[str]:
-        return [message]
+    def echo_skill(message: str, state: int) -> Tuple[List[str], int]:
+        return [message], 0
 
-    def skill_classifier(message: str) -> List[Callable[[str], List[str]]]:
+    def skill_classifier(message: str) -> List[Callable[[str], Tuple[List[str], int]]]:
         return [echo_skill]
 
     agent = Agent(skill_classifier=skill_classifier)
@@ -21,10 +21,10 @@ def test_echo_agent():
 
 
 def test_duplicate_agent():
-    def duplicate_skill(message: str) -> List[str]:
-        return [message * 2]
+    def duplicate_skill(message: str, state: int) -> Tuple[List[str], int]:
+        return [message * 2], 0
 
-    def skill_classifier(message: str) -> List[Callable[[str], List[str]]]:
+    def skill_classifier(message: str) -> List[Callable[[str], Tuple[List[str], int]]]:
         return [duplicate_skill]
 
     agent = Agent(skill_classifier=skill_classifier)
@@ -41,13 +41,13 @@ def test_error_type_of_skill_classifier():
 
 
 def test_choice_of_skills():
-    def greeting_skill(message: str) -> List[str]:
-        return ['Hi']
+    def greeting_skill(message: str, state: int) -> Tuple[List[str], int]:
+        return ['Hi'], 0
 
-    def parting_skill(message: str) -> List[str]:
-        return ['Bye']
+    def parting_skill(message: str, state: int) -> Tuple[List[str], int]:
+        return ['Bye'], 0
 
-    def skill_classifier(message: str) -> List[Callable[[str], List[str]]]:
+    def skill_classifier(message: str) -> List[Callable[[str], Tuple[List[str], int]]]:
         skills = []
         if 'Hello' in message:
             skills.append(greeting_skill)
@@ -78,3 +78,19 @@ def test_choice_of_skills():
     output_message = agent.answer_me(input_message)
 
     assert output_message == []
+
+
+def test_continuous_skill():
+    def continuous_skill(message: str, state: int) -> Tuple[List[str], int]:
+        if state == 0:
+            return ['How old are you?'], 1
+        if state == 1:
+            return ['Ok'], 0
+
+    def skill_classifier(message: str) -> List[Callable[[str], Tuple[List[str], int]]]:
+        return [continuous_skill]
+
+    agent = Agent(skill_classifier=skill_classifier)
+
+    assert agent.answer_me('Hello') == ['How old are you?']
+    assert agent.answer_me('23') == ['Ok']

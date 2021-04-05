c7fcb9e85f6c8176c71fdd71ff00173e1eedc10e
Vyacheslav Rusov <odryfox@gmail.com>
2021-04-05 22:01:34 +0300

    Refactor quotes

--- setup.py
+++ setup.py
@@ -4,36 +4,36 @@ import re
 from setuptools import find_packages
 from setuptools import setup
 
-with io.open("README.rst", "rt", encoding="utf8") as f:
+with io.open('README.rst', 'rt', encoding='utf8') as f:
     readme = f.read()
 
-with io.open("src/millet/__init__.py", "rt", encoding="utf8") as f:
+with io.open('src/millet/__init__.py', 'rt', encoding='utf8') as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
 
 setup(
-    name="Millet",
+    name='Millet',
     version=version,
     project_urls={
-        "Code": "https://github.com/odryfox/millet",
-        "Issue tracker": "https://github.com/odryfox/millet/issues",
+        'Code': 'https://github.com/odryfox/millet',
+        'Issue tracker': 'https://github.com/odryfox/millet/issues',
     },
-    license="BSD-3-Clause",
-    author="Vyacheslav Rusov",
-    author_email="odryfox@gmail.com",
-    description="A simple framework for building complex dialogue systems.",
+    license='BSD-3-Clause',
+    author='Vyacheslav Rusov',
+    author_email='odryfox@gmail.com',
+    description='A simple framework for building complex dialogue systems.',
     long_description=readme,
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     include_package_data=True,
     extras_require={
-        "dev": [
-            "pytest",
-            "mkdocs",
-            "redis",
-            "isort",
+        'dev': [
+            'pytest',
+            'mkdocs',
+            'redis',
+            'isort',
         ],
-        "docs": [
-            "mkdocs",
+        'docs': [
+            'mkdocs',
         ],
     },
 )
